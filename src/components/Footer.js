bf956a6539b375f1feca0f89cd6858af809ca2bd
Royroad <imyeongrok@woowayouths.com>
2025-10-21 23:55:26 +0900

    Remove address and contact info from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,6 @@ const Footer = () => {
   return (
     <footer className="bg-dark text-white text-center py-4">
       <div className="container">
-        <div className="mb-3">
-          <p className="mb-1">서울 송파구 백제고분로 364 대신빌딩 3층 (배민아카데미)</p>
-          <p className="mb-1">문의: 카카오톡 채널(@배민커넥트)</p>
-        </div>
-        
         {/* 제안 안내 메시지 */}
         <div className="footer-suggestion-note">
           <small className="text-light">
